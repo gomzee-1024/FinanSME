c46437dd2ae8c1ce3124df1a25b6a44fda509c31
gautam.kakadiya <gautam.kakadiya@power2sme.com>
2016-10-26 18:07:50 +0530

    improved some things

--- app/src/main/java/com/power2sme/fsme/finansme/MainActivity.java
+++ app/src/main/java/com/power2sme/fsme/finansme/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -21,34 +24,15 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
     private ViewPager mPager;
-    //private YourPagerAdapter mAdapter;
+    private MyPagerAdapter mAdapter;
     private TabLayout mTabLayout;
-
+    private MyAccountFragment myAccountFrag,appliedLoansFrag;
+    private FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(mToolbar);
-
-        mCoordinator = (CoordinatorLayout) findViewById(R.id.root_coordinator);
-        mFab = (FloatingActionButton) findViewById(R.id.fab);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.drawer_open, R.string.drawer_close);
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
-        mDrawerToggle.syncState();
-        mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        //mAdapter = new YourPagerAdapter(getSupportFragmentManager());
-        //mPager = (ViewPager) findViewById(R.id.view_pager);
-        //mPager.setAdapter(mAdapter);
-        //Notice how the Tab Layout links with the Pager Adapter
-        //mTabLayout.setTabsFromPagerAdapter(mAdapter);
-
-        //Notice how The Tab Layout adn View Pager object are linked
-        //mTabLayout.setupWithViewPager(mPager);
-        //mPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        initUi();
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -58,5 +42,59 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void initUi() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setLogo(R.drawable.ic_p2s_logo);
+        mToolbar.setContentInsetStartWithNavigation(200);
+        setSupportActionBar(mToolbar);
+        myAccountFrag = MyAccountFragment.newInstance("","");
+        appliedLoansFrag = MyAccountFragment.newInstance("","");
+        mCoordinator = (CoordinatorLayout) findViewById(R.id.root_coordinator);
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.drawer_open, R.string.drawer_close);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+        mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        mAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        mPager = (ViewPager) findViewById(R.id.view_pager);
+        mPager.setAdapter(mAdapter);
+        //Notice how the Tab Layout links with the Pager Adapter
+        mTabLayout.setTabsFromPagerAdapter(mAdapter);
 
+        //Notice how The Tab Layout adn View Pager object are linked
+        mTabLayout.setupWithViewPager(mPager);
+        mPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+    }
+
+    class MyPagerAdapter extends FragmentStatePagerAdapter {
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if(position==0){
+                return myAccountFrag;
+            }else{
+                return appliedLoansFrag;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            if(position==0){
+                return "MY ACCOUNT";
+            }else{
+                return "APPLIED LOANS";
+            }
+        }
+    }
 }
