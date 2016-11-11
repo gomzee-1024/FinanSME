43d1debfd7a360bd6924c1b8a2479ebb92dd136f
gautam.kakadiya <gautam.kakadiya@power2sme.com>
2016-11-11 20:30:03 +0530

    added few myaccount layouts

--- app/src/main/java/com/power2sme/fsme/finansme/MyAccountFragment.java
+++ app/src/main/java/com/power2sme/fsme/finansme/MyAccountFragment.java
@@ -58,7 +58,7 @@ public class MyAccountFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_common, container, false);
+        return inflater.inflate(R.layout.fragment_common2, container, false);
     }
 
 
